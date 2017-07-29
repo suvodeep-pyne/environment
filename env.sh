2a3524cd16529660f4e9b6c07c8f7a3d00d4d227
Suvodeep Pyne <pyne.suvodeep@gmail.com>
2017-07-29 09:59:26 -0700

    updated env

--- env.sh
+++ env.sh
@@ -8,13 +8,4 @@ alias ...="cd ../../"
 alias .4="cd ../../../"
 alias .5="cd ../../../../"
 
-alias GRADLE_DEFAULT="~/.gradle/ligradle/gradle"
-gradle() {
-    if [ -f "./gradlew" ];
-    then
-        ./gradlew "$@"
-    else
-        GRADLE_DEFAULT "$@"
-    fi
-}
-
+alias gradle="./gradlew"
